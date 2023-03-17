c13e42862eb7b285677df2d8d98f145561000cfb
michaelgerman <mgerman@terpmail.umd.edu>
2023-03-16 22:50:01 -0400

    add all routes

--- app.py
+++ app.py
@@ -22,5 +22,31 @@ def ContactUsPage():
 def LogInScreen():
     return render_template('Log-In-Screen.html')
 
+@app.route('/CheckOut')
+def CheckOut():
+    return render_template('CheckoutPage.html')
+
+@app.route('/GenericProduct')
+def GenProduct():
+    return render_template('GenericProductPage.html')
+
+@app.route('/Inventory')
+def Inventory():
+    return render_template('Inventory Management.html')
+
+@app.route('/OrderConfim')
+def OrderConfirm():
+    return render_template('Order Confirmation.html')
+
+@app.route('/OrderDetails')
+def OrderDetails():
+    return render_template('OrderDetails.html')
+
+
+@app.route('/SalesTracker')
+def SalesTracker():
+    return render_template('Sales Tracker.html')
+
+
 if __name__ == '__main__':
     app.run()
