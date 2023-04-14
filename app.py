e127d6b73badcd2f2ec71d63d32542c81dfe44c6
joescope <joescope@terpmail.umd.edu>
2023-04-14 16:41:04 -0400

    Began authentication and authorization, database was created in create_db.py, began working on Inventory Management.html functionality with database.

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from werkzeug.security import check_password_hash
+from authorize import role_required
+from models import *
+from datetime import date as dt
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'ATB2.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'beyond_course_scope'
+db.init_app(app)
+
+
+login_manager = LoginManager()
+login_manager.login_view = 'LogIn' # default login route
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(username):
+   return Credentials.query.get(username)
 
 @app.route('/')
 def homePage():
@@ -26,11 +50,62 @@ def Reviews():
     return render_template('Reviews.html')
 
 @app.route('/LogIn', methods = ['GET', 'POST'])
-def LogInScreen():
-    if request.method == 'POST':
-        return render_template('Log-In-Screen.html', form_submitted=True)
-    else:
-        return render_template('Log-In-Screen.html')
+# def LogInScreen():
+#     if request.method == 'POST':
+#         return render_template('Log-In-Screen.html', form_submitted=True)
+#     else:
+#         return render_template('Log-In-Screen.html')
+def LogIn():
+   default_route_function = 'Shop'
+   default_student_route_function = 'homePage'
+
+
+   if request.method == 'GET':
+       # Determine where to redirect user if they are already logged in
+       if current_user and current_user.is_authenticated:
+           if current_user.role in ['MANAGER', 'ADMIN']:
+               return redirect(url_for(default_route_function))
+           elif current_user.role == 'STUDENT':
+               return redirect(url_for(default_student_route_function, user_id=0))
+       else:
+           redirect_route = request.args.get('next')
+           return render_template('Log-In-Screen.html', redirect_route=redirect_route)
+
+
+   elif request.method == 'POST':
+       username = request.form.get('username')
+       password = request.form.get('password')
+       redirect_route = request.form.get('redirect_route')
+
+
+       user = Credentials.query.filter_by(username=username).first()
+
+
+       # Validate user credentials and redirect them to initial destination
+       if user and check_password_hash(user.password, password):
+           login_user(user, force=True)
+
+
+           if current_user.role in ['MANAGER', 'ADMIN']:
+               return redirect(redirect_route if redirect_route else url_for(default_route_function))
+           elif current_user.role == 'STUDENT':
+               return redirect(redirect_route if redirect_route else url_for(default_student_route_function, user_id=0))
+       else:
+           flash(f'Your login information was not correct. Please try again.', 'error')
+
+
+       return redirect(url_for('LogIn'))
+
+
+   return redirect(url_for('LogIn'))
+
+
+@app.route('/logout')
+@login_required
+def logout():
+   logout_user()
+   flash(f'You have been logged out.', 'success')
+   return redirect(url_for('homePage'))
 
 @app.route('/CheckOut', methods=['GET', 'POST'])
 def CheckOut():
@@ -45,11 +120,36 @@ def GenProduct():
     return render_template('GenericProductPage.html')
 
 @app.route('/InventoryInput')
-def InventoryInput():
-    if request.method == 'POST':
-        return render_template('Inventory Management.html', form_submitted=True)
-    else:
-        return render_template('Inventory Management.html')
+@login_required
+@role_required(['ADMIN', 'MANAGER'])
+# def Inventory():
+#     return render_template('Inventory Management.html')
+
+def inventory_entry():
+   if request.method == 'GET':
+       sizes = Inventory.query.order_by(Inventory.size) \
+           .order_by(Inventory.size) \
+           .all()
+       return render_template('Inventory Management.html', sizes=sizes, action='create')
+   elif request.method == 'POST':
+       item_name = request.form['item_name']
+       size = request.form['size']
+       color = request.form['color']
+       price = request.form['price']
+       quantity = request.form['quantity']
+       description = request.form['description']
+
+       items = Inventory(item_name=item_name, size=size, color=color, price=price,
+                         quantity=quantity, description=description)
+       db.session.add(items)
+       db.session.commit()
+       flash(f'{item_name} was successfully added!', 'success')
+       return redirect(url_for('InventoryLog'))
+
+
+   # Address issue where unsupported HTTP request method is attempted
+   flash(f'Invalid request. Please contact support if this problem persists.', 'error')
+   return redirect(url_for('InventoryLog'))
 
 @app.route('/OrderConfirm')
 def OrderConfirm():
--- models.py
+++ models.py
@@ -1,12 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 db = SQLAlchemy()
 
 
-class Customer(db.Model):
-   __tablename__ = "Customer"
+class User(UserMixin, db.Model):
+   __tablename__ = "User"
 
-
-   customer_id = db.Column(db.Integer, primary_key=True)
+   user_id = db.Column(db.Integer, primary_key=True)
    first_name = db.Column(db.String(30), nullable=False)
    last_name = db.Column(db.String(50), nullable=False)
    email = db.Column(db.String(60), nullable=False)
@@ -33,7 +33,7 @@ class Requests(db.Model):
 
 
    request_id = db.Column(db.Integer, primary_key=True)
-   customer_id = db.Column(db.Integer, db.ForeignKey('Customer.customer_id'))
+   customer_id = db.Column(db.Integer, db.ForeignKey('User.user_id'))
    request_info = db.Column(db.String(200), nullable=False)
 
 
@@ -50,29 +50,39 @@ class OrderInfo(db.Model):
    __tablename__ = "OrderInfo"
 
    order_id = db.Column(db.Integer, primary_key=True)
-   customer_id = db.Column(db.Integer, db.ForeignKey('Customer.customer_id'))
+   customer_id = db.Column(db.Integer, db.ForeignKey('User.user_id'))
    shipping_mode = db.Column(db.String(30), nullable=False)
    order_total = db.Column(db.Float, nullable=False)
-   date = db.Column(db.date, nullable=False)
-
-
-    def __init__(self, customer_id, shipping_mode, order_total, date) :
+   def __init__(self, customer_id, shipping_mode, order_total, date) :
        self.customer_id = customer_id
        self.shipping_mode = shipping_mode
        self.order_total = order_total
        self.date = date
-
-
-    def __repr__(self):
+   def __repr__(self):
        return f"{self.shipping_mode}"
 
+class Size(db.Model):
+   __tablename__ = "Size"
+
+
+   size_id = db.Column(db.Integer, primary_key=True)
+   size = db.Column(db.String(30), nullable=False)
+   items = db.relationship('Inventory', backref='items')
+
+
+   def __init__(self, size):
+       self.size = size
+
+
+   def __repr__(self):
+       return f"{self.size}"
 
 class Inventory(db.Model):
     __tablename__ = "Inventory"
 
     product_id = db.Column(db.Integer, primary_key=True)
     collection_id = db.Column(db.Integer, db.ForeignKey('Collections.collection_id'))
-    size = db.Column(db.String(6), nullable=False)
+    size = db.Column(db.String(6), db.ForeignKey('Size.size_id'), nullable=False)
     color = db.Column(db.String(12), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
 
@@ -83,8 +93,8 @@ class Inventory(db.Model):
         self.quantity = quantity
 
 
-def __repr__(self):
-    return f"{self.size} {self.color} {self.quantity}"
+    def __repr__(self):
+        return f"{self.size} {self.color} {self.quantity}"
 
 
 
@@ -106,16 +116,28 @@ class Collections(db.Model):
     collection_name = db.Column(db.String(20), nullable=False)
 
     def __init__(self, collection_name) :
-        self.collection_namw = collection_name
-
-   def __repr__(self):
+        self.collection_name = collection_name
+    def __repr__(self):
        return f"{self.collection_name}"
 
 
-class LogIn(db.Model):
-    __tablename__ = "LogIn"
+class Credentials(db.Model):
+    __tablename__ = "Credentials"
 
     username = db.Column(db.String(40), primary_key=True)
     password = db.Column(db.String(40), nullable=False)
-   # Only execute once! Initial load of majors
-   #db.engine.execute("INSERT INTO major(major) VALUES ('Accounting');")
+    role = db.Column(db.String(10), nullable=False)
+
+    def __init__(self, username, password, role='PUBLIC'):
+        self.username = username
+        self.password = password
+        self.role = role
+
+    def get_id(self):
+        return (self.username)
+
+    def __repr__(self):
+       return f"({self.username}){self.password}{self.role}"
+
+   # Only execute once! Initial load of sizes
+   #db.engine.execute("INSERT INTO sizes(size) VALUES ('XS');")
